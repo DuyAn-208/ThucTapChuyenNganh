e812338bcf0129f3e6e25a18994f327db33c7e44
Nguyen Duy An <nguyenduyan200804@example.com>
2025-11-09 18:02:59 +0700

    Báo Cáo Tuần 4

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -3,22 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const {engine} = require('express-handlebars');
+var app = express();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var aboutRouter = require('./routes/about');
-var blogRouter = require('./routes/blog');
-var blog_detailsRouter = require('./routes/blog-details');
-var checkoutRouter = require('./routes/checkout');
-var classRouter = require('./routes/class');
-var contactRouter = require('./routes/contact');
-var shop_detailsRouter = require('./routes/shop-details');
-var shopping_cartRouter = require('./routes/shopping-cart');
-var shopRouter = require('./routes/shop');
-var wishlistRouter = require('./routes/wishlist');
-var app = express();
+var adminRouter = require('./routes/admin');
 
-// view engine setup
+
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'layouts',
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    layoutsDir: path.join(__dirname, 'views', 'layouts')
+}));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -30,31 +29,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/shop', shopRouter);
-app.use('/about', aboutRouter);
-app.use('/blog', blogRouter);
-app.use('/blog-details', blog_detailsRouter);
-app.use('/checkout', checkoutRouter);
-app.use('/class', classRouter);
-app.use('/contact', contactRouter);
-app.use('/shop-details', shop_detailsRouter);
-app.use('/shopping-cart', shopping_cartRouter);
-app.use('/wishlist', wishlistRouter);
+app.use('/admin', adminRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- NodeJs/routes/index.js
+++ NodeJs/routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+router.all('/*', function(req, res, next) {
+    res.app.locals.layout = 'home';
+    next();
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('home/index', { title: 'Home Page' });
+});
+router.get('/shop', function(req, res, next) {
+    res.render('home/shop', { title: 'product Page' });
 });
 
+router.get('/about', function(req, res, next) {
+    res.render('home/about', { title: 'about Page' });
+});
+router.get('/contact', function(req, res, next) {
+    res.render('home/contact', { title: 'Contact Page' });
+});
+router.get('/shop-details', function(req, res, next) {
+    res.render('home/shop-details', { title: 'product-detail Page' });
+});
+router.get('/shopping-cart', function(req, res, next) {
+    res.render('home/shopping-cart', { title: 'shopping-cart Page' }); // -> sẽ tự nhúng vào {{{body}}} trong layout.hbs
+});
+router.get('/blog', function(req, res, next) {
+    res.render('home/blog', { title: 'blog Page' }); // -> sẽ tự nhúng vào {{{body}}} trong layout.hbs
+});
+router.get('/blog-details', function(req, res, next) {
+    res.render('home/blog-details', { title: 'blog-detail Page' }); // -> sẽ tự nhúng vào {{{body}}} trong layout.hbs
+});
+router.get('/checkout', function(req, res, next) {
+    res.render('home/checkout', { title: 'blog-detail Page' }); // -> sẽ tự nhúng vào {{{body}}} trong layout.hbs
+});
+router.get('/wishlist', function(req, res, next) {
+    res.render('home/wishlist', { title: 'blog-detail Page' }); // -> sẽ tự nhúng vào {{{body}}} trong layout.hbs
+});
+router.get('/class', function(req, res, next) {
+    res.render('home/class', { title: 'blog-detail Page' }); // -> sẽ tự nhúng vào {{{body}}} trong layout.hbs
+});
 module.exports = router;
+
+
